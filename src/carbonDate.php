39aac6974f1c040cbea66c4917a158c0a1e48464
Saad Iqbal <iqbal.saad@travelbrands.com>
2024-01-12 12:38:30 -0500

    more carbon readings

--- src/carbonDate.php
+++ src/carbonDate.php
@@ -11,4 +11,14 @@
     echo $tomorrow->locale('en')->isoFormat('dddd, MMMM Do YYYY, h:mm');
     echo "<br />";
     echo Carbon::now()->subMinutes(4)->diffForHumans();
+    echo "<br />";
+    $year = 2024; $month = 1; $day = 12; $hour = 2; $minute = 30; $second = 15; $tz = 'America/Toronto';
+    echo Carbon::createFromDate($year, $month, $day, $tz)."<br />";
+    echo Carbon::createMidnightDate($year, $month, $day, $tz)."<br />";
+    echo Carbon::createFromTime($hour, $minute, $second, $tz)."<br />";
+    echo Carbon::createFromTimeString("$hour:$minute:$second", $tz)."<br />";
+    echo Carbon::create($year, $month, $day, $hour, $minute, $second, $tz)."<br />";
+    echo Carbon::createFromTimeString('2:00:00', 'America/Toronto');
+
+    
 ?>
